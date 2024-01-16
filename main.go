2c9f4c55cfaccd8b05777da8fe6d09ffe053781e
ILS - Industrielogistik <marcel.jaworski@ils-gmbh.net>
2024-01-16 09:36:00 +0100

    context.WithCancel

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func doSomething(ctx context.Context) {
+	// ctx, cancelCtx := context.WithCancel(ctx)
+
+	deadline := time.Now().Add(1500 * time.Millisecond)
+	ctx, cancelCtx := context.WithDeadline(ctx, deadline)
+	defer cancelCtx()
+
+	printCh := make(chan int)
+	go doAnother(ctx, printCh)
+
+	for num := 1; num <= 3; num++ {
+		printCh <- num
+	}
+
+	cancelCtx()
+
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Printf("doSomething: finished\n")
+}
+
+func doAnother(ctx context.Context, printCh <-chan int) {
+	for {
+		select {
+		case <-ctx.Done():
+			if err := ctx.Err(); err != nil {
+				fmt.Printf("doAnother err: %s\n", err)
+			}
+			fmt.Printf("doAnother: finished\n")
+			return
+		case num := <-printCh:
+			fmt.Printf("doAnother: %d\n", num)
+		}
+	}
+}
+
+func main() {
+	ctx := context.Background()
+
+	ctx = context.WithValue(ctx, "myKey", "myValue")
+
+	doSomething(ctx)
+
+}
